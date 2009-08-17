d4c6f2c4585efc69a3a42ddec3dcfa6c67accff3
Konstantin Saurbier <konstantin@saurbier.net>
2009-08-17 07:56:45 +0200

    Changed 3-clause BSD license to 2-clause variant.

--- lib/Sconnections.rb
+++ lib/Sconnections.rb
@@ -11,8 +11,6 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. The name of the author may not be used to endorse or promote products
-#    derived from this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -57,20 +55,24 @@ class Sconnections
     @data['udp'] = 0
     @data['tcp'] = 0
 
-    if(@config['Smain']['os'] == "freebsd6" || @config['Smain']['os'] == "linux2.6")
-      output = %x[netstat -n]
-      output.each do |line|
-        if(line =~ /tcp/)
-          @data['tcp'] += 1
-        elsif(line =~ /udp/)
-          @data['udp'] += 1
-        end
+    output = %x[netstat -n]
+    output.each do |line|
+      if(line =~ /tcp/)
+        @data['tcp'] += 1
+      elsif(line =~ /udp/)
+        @data['udp'] += 1
       end
     end
+
+    return @data
   end
 
   def write
-    RRD.update(@rrdname, "N:#{@data['tcp']}:#{@data['udp']}")
+    if(RRD.update(@rrdname, "N:#{@data['tcp']}:#{@data['udp']}"))
+      return true
+    else
+      return false
+    end
   end
 
   def graph(timeframe, filename = nil)
